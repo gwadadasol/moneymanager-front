648e1646c876637855f77f41b058c7d787fa0c4d
philippe anselme-vatin <philippe.anselmevatin@gmail.com>
2021-11-13 05:45:33 -0500

    Azure sql db

--- backend/MoneyManagerBackend/CategoryService/Startup.cs
+++ backend/MoneyManagerBackend/CategoryService/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CategoryService.AsyncDataServices;
 using CategoryService.Domains.Model;
 using CategoryService.Domains.Repository;
 using MediatR;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,7 +25,7 @@ namespace CategoryService
 
         public IConfiguration Configuration { get; }
         private readonly IWebHostEnvironment _env;
-                public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
             _env = env;
@@ -33,22 +35,27 @@ namespace CategoryService
         public void ConfigureServices(IServiceCollection services)
         {
 
-               if (_env.IsDevelopment())
-            {
-                Console.WriteLine("Development Mode");
-                Console.WriteLine("Use In Memory DB");
-                services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
-            }
-            else
-            {
-                Console.WriteLine("Production Mode");
-                services.AddDbContext<AppDbContext>(opt =>
-                opt.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
-            }
+            //    if (_env.IsDevelopment())
+            // {
+            //     Console.WriteLine("Development Mode");
+            //     Console.WriteLine("Use In Memory DB");
+            //     services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+            // }
+            // else
+            // {
+            Console.WriteLine("Production Mode");
+
+            var conStrBuilder = new SqlConnectionStringBuilder(Configuration.GetConnectionString("SqlServer"));
+            conStrBuilder.Password = Configuration["AzureSQLPassword"];
+            var connection = conStrBuilder.ConnectionString; 
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connection));
+
+            
+            // services.Configure<AzureServiceBusConfiguration>(Configuration.GetSection("AzureServiceBus"));
 
             services.AddMediatR(typeof(Startup));
             services.AddControllers();
-            
+
             services.AddScoped<ICategoryRepository, CategoryRepository>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -58,12 +65,12 @@ namespace CategoryService
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CategoryService", Version = "v1" });
             });
 
-              services.AddCors(options => options.AddDefaultPolicy(
-                //name: "MyPolicy",
-                builder =>
-                {
-                    builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
-                }));
+            services.AddCors(options => options.AddDefaultPolicy(
+              //name: "MyPolicy",
+              builder =>
+              {
+                  builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+              }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
